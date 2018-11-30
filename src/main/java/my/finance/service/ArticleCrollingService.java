112107413bbd4111fb0f15fbd3e565cce636a06e
DESKTOP-1ID017H\jhchoi <timulys@gmail.com>
2018-11-30 23:11:59 +0900

    Crolling 기능 테스트(진행중...)

--- src/main/java/my/finance/service/ArticleCrollingService.java
+++ src/main/java/my/finance/service/ArticleCrollingService.java
@@ -1,13 +1,18 @@
 package my.finance.service;
 
-import my.finance.dao.ArticleRepository;
-import my.finance.domain.Article;
-import my.finance.domain.PaperMap;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import my.finance.dao.ArticleRepository;
+import my.finance.domain.Article;
+import my.finance.domain.PaperMap;
 
 @Service
 public class ArticleCrollingService extends CrollingService {
@@ -16,11 +21,32 @@ public class ArticleCrollingService extends CrollingService {
     @Autowired
     private ArticleRepository articleRepository;
 
-    public List<Article> getArticleList(String newspaperCode) {
+    public List<Article> getArticleList() {
         List<Article> articles = new ArrayList<>();
-        /*getDocument(url);*/
 
+        Map<String, String> map = paperMap.getUrlMap();
+        map.values().parallelStream().forEach(url -> {
+        	try {
+				Document document = getDocument(url);
+				crollingArticle(document);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+        });
+        
         return articles;
     }
+    
+    private List<Article> crollingArticle(Document doc) {
+    	
+    	
+    	return null;
+    }
+    
+    public static void main(String[] args) throws IOException {
+		Document doc = Jsoup.connect("http://news.mt.co.kr/newsflash/newsflash.html?sec=invest&pDepth=&page=1&listType=left").get();
+		System.out.println(doc);
+	}
+    
 
 }
